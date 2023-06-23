2aeff46e869966e63443a40f8540eb0ad94bc242
betty <bxzr10@gmail.com>
2023-06-23 14:56:05 -0700

    new project adoptapet

---
+++ src/components/search/index.js
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react';
+import { createSearchParams, useNavigate } from 'react-router-dom';
+
+const Search = () => {
+
+    const navigate = useNavigate();
+
+    const searchInputRef = useRef();
+
+    const onSearchHandler = (e) => {
+        e.preventDefault();
+
+        const searchQuery = {
+            name: searchInputRef.current.value
+        }
+
+        // use createSearchParams 
+        const query = createSearchParams(searchQuery);
+
+        // imperatively redirect with useNavigate() returned function
+        navigate(`/search?${query}`);
+    };
+
+    return (
+        <form onSubmit={onSearchHandler} className="search-form">
+        <input type="text" className="search" ref={searchInputRef} />
+        <button type="submit" className="search-button">
+            🔎
+        </button>
+        </form>
+    );
+};
+
+export default Search;
